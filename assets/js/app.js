811da57afa43dd50876bb1f9dc2c657f76c188bb
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2021-06-30 09:45:08 -0500

    Fixed bug at close menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,4 +16,11 @@ iconoMenu.addEventListener('click', () => {
 
 menu.addEventListener('click', () => {
   menu.classList.toggle('header__container-menu--active');
+  if (iconoMenu.className === "fas fa-bars") {
+    iconoMenu.classList.remove('fa-bars');
+    iconoMenu.classList.add('fa-times');
+  } else {
+    iconoMenu.classList.remove('fa-times');
+    iconoMenu.classList.add('fa-bars');
+  }
 })
