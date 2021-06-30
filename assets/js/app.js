a0f825d6389f7ebf0b9618dce76a9b1c3dac76e5
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2021-06-30 09:22:07 -0500

    First commit

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+const iconoMenu = document.getElementById('icono-menu'),
+  menu = document.getElementById('menu');
+
+
+iconoMenu.addEventListener('click', () => {
+  menu.classList.toggle('header__container-menu--active');
+
+  if (iconoMenu.className === "fas fa-bars") {
+    iconoMenu.classList.remove('fa-bars');
+    iconoMenu.classList.add('fa-times');
+  } else {
+    iconoMenu.classList.remove('fa-times');
+    iconoMenu.classList.add('fa-bars');
+  }
+});
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('header__container-menu--active');
+})
